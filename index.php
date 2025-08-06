6a15e8079b571154fc5ac0b00cabc3c670a522b6
Nicholas Tardie <194505030+ntardie26@users.noreply.github.com>
2025-08-06 15:49:21 -0400

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
     <form method="POST" action="validate_captcha.php">
-        <img src="generate_captcha.php" alt="CAPTCHA">
+        <img src="generate_captcha.php?t=<?php echo time(); ?>" alt="Captcha challenge" id="captcha-image" style="border: 1px solid #666; margin: 10px 0; display: block; margin-left: auto; margin-right: auto;">
         <br>
         <input type="text" name="captcha_input" required>
         <input type="submit" value="Submit">
